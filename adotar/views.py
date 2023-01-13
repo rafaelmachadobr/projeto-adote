00e5f7af65df34a249e6f1cc0af3ef1c21159bd1
Rafael <rafaelf.machado2004@gmail.com>
2023-01-13 16:51:43 -0300

    Ver pedidos de adoção

--- adotar/views.py
+++ adotar/views.py
@@ -4,6 +4,7 @@ from django.contrib.messages import constants
 from django.contrib import messages
 from .models import PedidoAdocao
 from datetime import datetime
+from django.core.mail import send_mail
 
 
 def listar_pets(request):
@@ -26,9 +27,10 @@ def listar_pets(request):
 
 def pedido_adocao(request, id_pet):
     pet = Pet.objects.filter(id=id_pet).filter(status="P")
-    
+
     if not pet.exists():
-        messages.add_message(request, constants.ERROR, 'Esse pet já foi adotado :)')
+        messages.add_message(request, constants.ERROR,
+                             'Esse pet já foi adotado :)')
         return redirect('/adotar')
 
     pedido = PedidoAdocao(pet=pet.first(),
@@ -40,3 +42,28 @@ def pedido_adocao(request, id_pet):
     messages.add_message(request, constants.SUCCESS,
                          'Pedido de adoção realizado, você receberá um e-mail caso ele seja aprovado.')
     return redirect('/adotar')
+
+
+def processa_pedido_adocao(request, id_pedido):
+    status = request.GET.get('status')
+    pedido = PedidoAdocao.objects.get(id=id_pedido)
+    if status == "A":
+        pedido.status = 'AP'
+        string = '''Olá, sua adoção foi aprovada. ...'''
+    elif status == "R":
+        string = '''Olá, sua adoção foi recusada. ...'''
+        pedido.status = 'R'
+
+    pedido.save()
+
+    print(pedido.usuario.email)
+    email = send_mail(
+        'Sua adoção foi processada',
+        string,
+        'adota@gmail.com',
+        [pedido.usuario.email, ],
+    )
+
+    messages.add_message(request, constants.SUCCESS,
+                         'Pedido de adoção processado com sucesso')
+    return redirect('/divulgar/ver_pedido_adocao')
